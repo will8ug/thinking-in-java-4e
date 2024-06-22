ecf409d7a6ac1dbe9bcd2622f345f358f53d5234
Will <yzj_000@163.com>
2024-06-22 19:44:28 +0800

    Simplify the original packages a bit

---
+++ src/main/java/net/will/thkij4ed/ch10/DotNewMoreTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.will.thkij4ed.ch10;
+
+/**
+ * @author Will
+ * @version 2012-9-6
+ */
+public class DotNewMoreTest {
+	public static void main(String[] args) {
+		DotNew dn = new DotNew();
+		DotNew.InnerDefault dnid = dn.new InnerDefault();
+		dnid.funny();
+	}
+}
---
+++ src/main/java/net/will/thkij4ed/ch10/Wrapping.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package net.will.thkij4ed.ch10;
+
+/**
+ * @author Will
+ * @version 2012-9-3
+ */
+public class Wrapping {
+	private int i;
+	public Wrapping(int i) { this.i = i; }
+	public int value() { return this.i; }
+}
